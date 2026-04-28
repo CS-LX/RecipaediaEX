3432de46a63c9b474d2ee3d9cb0a520afad028ef
LX.. <1659404657@qq.com>
2026-04-28 21:50:21 +0800

    适配1.9.0.2

---
+++ LoaderExtra/Loaders/RegularRecipesLoader.cs
@@ -0,0 +1,21 @@
+﻿using Game;
+using RecipaediaEX.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RecipaediaEX.Implementation {
+    public class RegularRecipesLoader : XmlRecipesLoader {
+        public override void Initialize() {
+            XElement xElement = null;
+            foreach (ModEntity modEntity in ModsManager.ModList) {
+                modEntity.LoadCr(ref xElement);
+            }
+            XElements.Add(xElement);
+        }
+        public override int Order => -20;
+    }
+}
